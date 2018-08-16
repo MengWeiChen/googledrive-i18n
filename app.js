16f3d1768b5d84973099af1be7aeb55953d5e1b1
Meng Chan <yses004004@gmail.com>
2018-08-16 21:46:21 +0800

    feature: add sheets mode

--- app.js
+++ app.js
@@ -1,121 +1,167 @@
 #!/usr/bin/env node
-const program = require("commander");
-const path = require("path");
-const stringify = require("json-stable-stringify");
+const program = require('commander')
+const path = require('path')
+const stringify = require('json-stable-stringify')
 
-const { getFileList, getSheetData, writeFile, getAuth } = require("./lib");
-let keyMap = require("./defaultKeyMap.json");
+const {
+  getFileList,
+  getSheetData,
+  writeFile,
+  getAuth,
+  getSheets
+} = require('./lib')
+let keyMap = require('./defaultKeyMap.json')
 
-const getKey = key => keyMap[key] || key;
+const getKey = key => keyMap[key] || key
 
 const assocPath = (paths, val, obj) => {
-  let pointer = null;
+  let pointer = null
   paths.forEach((path, index) => {
-    if (pointer === null) pointer = obj;
-    if (!pointer[path] && index !== paths.length - 1) pointer[path] = {};
-    if (index === paths.length - 1) pointer[path] = val;
-    pointer = pointer[path];
-  });
-};
+    if (pointer === null) pointer = obj
+    if (!pointer[path] && index !== paths.length - 1) pointer[path] = {}
+    if (index === paths.length - 1) pointer[path] = val
+    pointer = pointer[path]
+  })
+}
 
 const outputFiles = async (outputPath, data) => {
   for (const key of Object.keys(data)) {
     await writeFile(
       `${outputPath}/${key}.json`,
-      stringify(data[key], { space: "\t" })
-    );
+      stringify(data[key], { space: '\t' })
+    )
   }
-};
+}
+const scanSheet = async (auth, sheetId) => {
+  let result = {}
+  const sheets = await getSheets(auth, sheetId)
+  console.log(sheets)
+  const gridNameList = sheets.map(s => s.properties.title)
+  const keymapName = '_keymap'
+  if (gridNameList.some(name => name == keymapName)) {
+    keyMap = await parseKeymap(auth, sheetId, keymapName)
+  }
+  const targetGridName = gridNameList.filter(
+    gridNmae => gridNmae.indexOf('_') !== 0
+  )
 
-const parseKeymap = async (auth, keymapSheetId) => {
-  const keyMap = {};
-  const res = await getSheetData(auth, keymapSheetId);
-  const [keyRow, ...rows] = res;
+  for (const gridName of targetGridName) {
+    const res = await getSheetData(auth, sheetId, gridName)
+    console.log(res);
+    const [languages, ...strings] = res
+    
+
+    strings.forEach(row => {
+      const [key, ...contents] = row
+      contents.forEach((string, index) => {
+        const lang = getKey(languages[index + 1])
+        if (lang.indexOf('_') == 0) return
+        if (key == '') return
+        assocPath([lang, gridName, key], string, result)
+      })
+    })
+  }
+
+  return result
+}
+
+const parseKeymap = async (auth, keymapSheetId, gridName) => {
+  const keyMap = {}
+  const res = await getSheetData(auth, keymapSheetId, gridName)
+  const [keyRow, ...rows] = res
   rows.forEach(row =>
     row.forEach((d, i) => {
-      if (i === 0) return;
-      keyMap[d] = keyRow[i];
+      if (i === 0) return
+      keyMap[d] = keyRow[i]
     })
-  );
-  return keyMap;
-};
+  )
+  return keyMap
+}
 
 const keyMapFilter = d =>
-  d.mimeType === "application/vnd.google-apps.spreadsheet" &&
-  d.name.indexOf("_keymap") === 0;
+  d.mimeType === 'application/vnd.google-apps.spreadsheet' &&
+  d.name.indexOf('_keymap') === 0
 
 const folderFilter = d =>
-  d.mimeType === "application/vnd.google-apps.folder" &&
-  d.name.indexOf("_") !== 0;
+  d.mimeType === 'application/vnd.google-apps.folder' &&
+  d.name.indexOf('_') !== 0
 
 const sheetFilter = d =>
-  d.mimeType === "application/vnd.google-apps.spreadsheet" &&
-  d.name.indexOf("_") !== 0;
+  d.mimeType === 'application/vnd.google-apps.spreadsheet' &&
+  d.name.indexOf('_') !== 0
 
 const scanFolder = async (auth, folderId, path = [], data) => {
-  let result = data ? data : {};
+  let result = data ? data : {}
 
-  const fileList = await getFileList(auth, folderId);
+  const fileList = await getFileList(auth, folderId)
 
-  const keymapSheet = fileList.find(keyMapFilter);
+  const keymapSheet = fileList.find(keyMapFilter)
   if (keymapSheet) {
-    keyMap = await parseKeymap(auth, keymapSheet.id);
+    keyMap = await parseKeymap(auth, keymapSheet.id)
   }
 
-  const folderList = fileList.filter(folderFilter);
+  const folderList = fileList.filter(folderFilter)
   for (const folder of folderList) {
-    result = await scanFolder(auth, folder.id, [...path, folder.name], data);
+    result = await scanFolder(auth, folder.id, [...path, folder.name], data)
   }
 
-  const sheetList = fileList.filter(sheetFilter);
+  const sheetList = fileList.filter(sheetFilter)
   for (const sheet of sheetList) {
-    const res = await getSheetData(auth, sheet.id);
-    const sheetName = sheet.name;
-    const [languages, ...strings] = res;
+    const res = await getSheetData(auth, sheet.id)
+    const sheetName = sheet.name
+    const [languages, ...strings] = res
     strings.forEach(row => {
-      const [key, ...contents] = row;
+      const [key, ...contents] = row
       contents.forEach((string, index) => {
-        const lang = getKey(languages[index + 1]);
-        if (lang.indexOf("_") == 0) return;
-        assocPath([lang, ...path, sheetName, key], string, result);
-      });
-    });
+        const lang = getKey(languages[index + 1])
+        if (lang.indexOf('_') == 0) return
+        if (key == '') return
+        assocPath([lang, ...path, sheetName, key], string, result)
+      })
+    })
   }
 
-  return result;
-};
+  return result
+}
 
-const main = async (authPath, outputPath, rootFolderId) => {
-  const auth = await getAuth(authPath);
-  const result = await scanFolder(auth, rootFolderId);
-  await outputFiles(outputPath, result);
-};
+const main = async (authPath, outputPath, rootFolderId, sheetId) => {
+  const auth = await getAuth(authPath)
+  let result
+  if (!sheetId) {
+    result = await scanFolder(auth, rootFolderId)
+  } else {
+    result = await scanSheet(auth, sheetId)
+  }
+  await outputFiles(outputPath, result)
+}
 
 program
-  .version("0.0.1")
-  .usage("[options]")
-  .option("-f, --folder <folderId>", "google drive folderId.")
+  .version('0.0.1')
+  .usage('[options]')
+  .option('-f, --folder <folderId>', 'google drive folderId.')
   .option(
-    "-k, --key <keyFilePath>",
+    '-k, --key <keyFilePath>',
     'google service account json file path. Default is "./service_account.json"'
   )
-  .option("-o, --out <outputPath>", "output path.")
-  .parse(process.argv);
+  .option('-o, --out <outputPath>', 'output path.')
+  .option('-s, --sheetId <sheetId>', 'sheets mode with sheetId.')
+  .parse(process.argv)
 
-if (program.folder) {
-  rootFolderId = program.folder;
-  const startTime = new Date();
+if (program.folder || program.sheetId) {
+  rootFolderId = program.folder
+  const startTime = new Date()
   main(
-    program.key || "./service_account.json",
-    program.out || "output",
-    program.folder
+    program.key || './service_account.json',
+    program.out || 'output',
+    program.folder,
+    program.sheetId
   ).then(() => {
-    console.log("Build Succeeded");
-    console.log(`Time: ${(new Date() - startTime) / 1000}s`);
-    console.log(`Path: ${path.resolve(program.out || "output")}`);
-    process.exit();
-  });
+    console.log('Build Succeeded')
+    console.log(`Time: ${(new Date() - startTime) / 1000}s`)
+    console.log(`Path: ${path.resolve(program.out || 'output')}`)
+    process.exit()
+  })
 } else {
-  program.outputHelp();
-  process.exit();
+  program.outputHelp()
+  process.exit()
 }
--- lib.js
+++ lib.js
@@ -1,66 +1,78 @@
-const { google } = require("googleapis");
-const gfs = require("mkfs");
-const path = require("path");
+const { google } = require('googleapis')
+const gfs = require('mkfs')
+const path = require('path')
 
 module.exports = {
   getFileList: (auth, folderId) =>
     new Promise((resolve, reject) => {
       const drive = google.drive({
-        version: "v3",
+        version: 'v3',
         auth
-      });
+      })
       drive.files.list(
         {
           q: `'${folderId}' in parents`,
           pageSize: 100,
-          fields: "nextPageToken, files(id, name, mimeType)"
+          fields: 'nextPageToken, files(id, name, mimeType)'
         },
         (err, res) => {
-          if (err) return reject(err);
-          const files = res.data.files;
-          resolve(files);
+          if (err) return reject(err)
+          const files = res.data.files
+          resolve(files)
         }
-      );
+      )
     }),
-  getSheetData: (auth, sheetId) =>
+  getSheets: (auth, sheetId) => new Promise((resolve, reject) => {
+    const sheets = google.sheets({ version: 'v4', auth })
+    sheets.spreadsheets.get({
+      spreadsheetId: sheetId
+    }, (err, res)=>{
+      if (err) return reject(err)
+      const sheets = res.data.sheets
+      resolve(sheets)
+      
+    })
+  }),
+
+  getSheetData: (auth, sheetId, gridName) =>
     new Promise((resolve, reject) => {
-      const sheets = google.sheets({ version: "v4", auth });
+      const sheets = google.sheets({ version: 'v4', auth })
       sheets.spreadsheets.values.get(
         {
           spreadsheetId: sheetId,
-          range: "A1:ZZ999"
+          range: gridName ? `'${encodeURIComponent(gridName)}'!A1:ZZ999` : 'A1:ZZ999'
         },
         (err, res) => {
-          if (err) return reject(err);
-          const rows = res.data.values;
-          resolve(rows);
+          if (err) return reject(err)
+          const rows = res.data.values
+          resolve(rows)
         }
-      );
+      )
     }),
   writeFile: (path, data) =>
     new Promise((resolve, reject) => {
-      gfs.writeFiles(path, data, "utf8", (err, data) => {
-        if (err) return reject(err);
-        resolve(data);
-      });
+      gfs.writeFiles(path, data, 'utf8', (err, data) => {
+        if (err) return reject(err)
+        resolve(data)
+      })
     }),
   getAuth: filePath =>
     new Promise((resolve, reject) => {
-      const privatekey = require(`${path.resolve(process.cwd(), filePath)}`);
+      const privatekey = require(`${path.resolve(process.cwd(), filePath)}`)
       const jwtClient = new google.auth.JWT(
         privatekey.client_email,
         null,
         privatekey.private_key,
         [
-          "https://www.googleapis.com/auth/spreadsheets",
-          "https://www.googleapis.com/auth/drive"
+          'https://www.googleapis.com/auth/spreadsheets',
+          'https://www.googleapis.com/auth/drive'
         ]
-      );
+      )
       jwtClient.authorize((err, tokens) => {
         if (err) {
-          return reject(err);
+          return reject(err)
         }
-        resolve(jwtClient);
-      });
+        resolve(jwtClient)
+      })
     })
-};
+}
